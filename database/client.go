72ff829f6258d5c18a6b477f12ad0f91b2f7b66f
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2023-06-29 20:40:20 +0200

    task: switch to SingularTable

--- database/client.go
+++ database/client.go
@@ -6,13 +6,18 @@ import (
 	"github.com/cngJo/golang-api-auth/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 var Instance *gorm.DB
 var dbError error
 
 func Connect(connectionString string) {
-	Instance, dbError = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	Instance, dbError = gorm.Open(mysql.Open(connectionString), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
 	if dbError != nil {
 		log.Fatal(dbError)
 		panic("failed to connect database")
